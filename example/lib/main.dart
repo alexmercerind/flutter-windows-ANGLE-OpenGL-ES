5cc4671a7f32a928a0a050769d13e6f025f45053
Hitesh Kumar Saini <saini123hitesh@gmail.com>
2022-07-07 17:53:55 +0530

    refactor: remove boilerplate

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
-
 import 'package:flutter/services.dart';
-import 'package:flutter_windows_angle_d3d_texture/flutter_windows_angle_d3d_texture.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,35 +13,20 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
-  final _flutterWindowsAngleD3dTexturePlugin = FlutterWindowsAngleD3dTexture();
-
   @override
   void initState() {
     super.initState();
     initPlatformState();
   }
 
-  // Platform messages are asynchronous, so we initialize in an async method.
   Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    // We also handle the message potentially returning null.
-    try {
-      platformVersion =
-          await _flutterWindowsAngleD3dTexturePlugin.getPlatformVersion() ?? 'Unknown platform version';
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      _platformVersion = platformVersion;
-    });
+    const channel =
+        MethodChannel('com.alexmercerind/flutter_windows_angle_d3d_texture');
+    await channel.invokeMethod('CreateID3D11Device', {});
+    await channel.invokeMethod('ID3D11Device::CreateTexture2D', {});
+    await channel.invokeMethod('eglCreatePbufferFromClientBuffer', {});
+    await channel.invokeMethod('eglBindTexImage', {});
+    await channel.invokeMethod('glDrawArrays', {});
   }
 
   @override
@@ -52,11 +34,9 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Plugin example app'),
+          title: const Text('flutter_windows_angle_d3d_texture'),
         ),
-        body: Center(
-          child: Text('Running on: $_platformVersion\n'),
-        ),
+        body: const Center(),
       ),
     );
   }
