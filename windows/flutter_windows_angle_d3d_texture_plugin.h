c51d170503be5a45e1ab67ede5165727e09aa563
Hitesh Kumar Saini <saini123hitesh@gmail.com>
2022-07-07 17:56:50 +0530

    feat: setup ID3D11Device/ID3D11Texture2D initialization & egl/gl

--- windows/flutter_windows_angle_d3d_texture_plugin.h
+++ windows/flutter_windows_angle_d3d_texture_plugin.h
@@ -1,11 +1,33 @@
 #ifndef FLUTTER_PLUGIN_FLUTTER_WINDOWS_ANGLE_D3D_TEXTURE_PLUGIN_H_
 #define FLUTTER_PLUGIN_FLUTTER_WINDOWS_ANGLE_D3D_TEXTURE_PLUGIN_H_
 
+#ifndef EGL_EGL_PROTOTYPES
+#define EGL_EGL_PROTOTYPES 1
+#endif
+
+// Flutter
 #include <flutter/method_channel.h>
 #include <flutter/plugin_registrar_windows.h>
 #include <flutter/texture_registrar.h>
-
+// Win32 / C++/WinRT / C++/CX
+#include <windows.h>
+#include <wrl.h>
+// Direct3D 11
+#include <d3d.h>
+#include <d3d11.h>
+#pragma comment(lib, "dxgi.lib")
+#pragma comment(lib, "d3d11.lib")
+// Standard C++ STL
+#include <iostream>
 #include <memory>
+// ANGLE
+#include <EGL/egl.h>
+#include <EGL/eglext.h>
+#include <EGL/eglplatform.h>
+#include <GLES2/gl2.h>
+#include <GLES2/gl2ext.h>
+
+using namespace Microsoft::WRL;
 
 namespace flutter_windows_angle_d3d_texture {
 
@@ -22,6 +44,7 @@ class FlutterWindowsAngleD3dTexturePlugin : public flutter::Plugin {
   static void RegisterWithRegistrar(flutter::PluginRegistrarWindows* registrar);
 
   FlutterWindowsAngleD3dTexturePlugin(
+      flutter::PluginRegistrarWindows* registrar,
       std::unique_ptr<flutter::MethodChannel<flutter::EncodableValue>> channel,
       flutter::TextureRegistrar* texture_registrar);
 
@@ -37,8 +60,16 @@ class FlutterWindowsAngleD3dTexturePlugin : public flutter::Plugin {
       const flutter::MethodCall<flutter::EncodableValue>& method_call,
       std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result);
 
-  flutter::TextureRegistrar* texture_registrar_;
+  flutter::PluginRegistrarWindows* registrar_ = nullptr;
   std::unique_ptr<flutter::MethodChannel<flutter::EncodableValue>> channel_;
+  flutter::TextureRegistrar* texture_registrar_ = nullptr;
+  ID3D11Device* d3d11_device_ = nullptr;
+  ID3D11DeviceContext* d3d11_device_context_ = nullptr;
+  ComPtr<ID3D11Texture2D> d3d11_texture_2D_;
+  HANDLE shared_handle_;
+  EGLSurface surface_ = EGL_NO_SURFACE;
+  EGLDisplay display_ = EGL_NO_DISPLAY;
+  EGLConfig config_ = NULL;
 };
 
 }  // namespace flutter_windows_angle_d3d_texture
